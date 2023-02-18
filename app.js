eb16ebc04c4896b87719277f6ba23af254052876
DJ <97788700+DJMONSTER0212@users.noreply.github.com>
2023-02-18 11:46:48 +0530

    Joined it with MongoDB using Mongoose and now performing all the CRUD operation in our list

---
+++ app.js
@@ -0,0 +1,129 @@
+//jshint esversion:6
+
+const express = require("express");
+const bodyParser = require("body-parser");
+const mongoose = require('mongoose')
+
+const app = express();
+app.set('view engine', 'ejs');
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.static("public"));
+const uri = "mongodb+srv://DJ0212:devansh%400212@cluster0.djsnamb.mongodb.net/?retryWrites=true&w=majority";
+mongoose.connect(uri, () => {
+  console.log("DB connected!")
+});
+
+const itemSchema = {
+  name: String
+}
+const Item = mongoose.model("Item", itemSchema)
+
+const item1 = new Item({
+  name: "Welcome to your todo list!"
+})
+const item2 = new Item({
+  name: "Hit + button to add a new item"
+})
+const item3 = new Item({
+  name: "<--  hit this to delete an item."
+})
+
+const defaultItems = [item1, item2, item3];
+
+// Item.insertMany(defaultItems,function(err){
+//   if(err){
+//     console.log(err)
+//   }
+//   else {
+//     console.log("Mission passed")
+//   }
+// })
+app.get("/", function (req, res) {
+
+  Item.find(function (err, founditems) {
+    console.log("Hello");
+    if (founditems.length === 0) {
+      Item.insertMany(defaultItems, function (err) {
+        if (err) {
+          console.log(err)
+        }
+        else {
+          console.log("Mission passed")
+        }
+      })
+      res.redirect('/')
+    }
+    if (err) {
+      console.log(err)
+    }
+    else {
+      console.log(founditems)
+      res.render("list", { listTitle: "Today", newListItems: founditems });
+    }
+
+  })
+
+});
+
+app.post("/", function (req, res) {
+
+  const itemName = req.body.newItem;
+  const newItem = new Item({
+    name: itemName
+  })
+  newItem.save();
+  res.redirect('/')
+});
+
+app.post("/delete", function (req, res) {
+  const checkedItemId = req.body.checkbox
+  Item.findByIdAndRemove(checkedItemId, (err) => {
+    if (err) {
+      console.log(err);
+    } else {
+      console.log("Removed Successfully");
+      res.redirect('/')
+    }
+  })
+});
+const listSchema = {
+  name: String,
+  items: [itemSchema]
+}
+const List = mongoose.model("List", listSchema)
+
+// app.get("/work", function(req,res){
+//   res.render("list", {listTitle: "Work List", newListItems: workItems});
+// });
+
+app.get("/:vari", function (req, res) {
+  const customListName = req.params.vari;
+  List.findOne({ name: customListName }, function (err, foundlist) {
+    if (!err) {
+      if (!foundlist) {
+        //create a new list
+        // console.log("It doesn't Exist");
+
+        const list = new List({
+          name: customListName,
+          items: defaultItems
+        });
+        list.save();
+        res.redirect('/'+customListName)
+      }
+      else {
+        // show an existing list
+        res.render("list",{listTitle:customListName,newListItems:foundlist.items})
+      }
+    }
+  });
+})
+
+// app.get("/about", function(req, res){
+//   res.render("about");
+// });
+
+app.listen(3000, function () {
+  console.log("Server started on port 3000");
+});
